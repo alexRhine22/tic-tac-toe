00b821c2b54a8554d838fb179b898558936d8c5f
alexRhine22 <alextherhine@gmail.com>
2020-04-08 14:51:53 -0700

    Started working on script

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+const box1 = document.getElementById('box-1');
+const box2 = document.getElementById('box-2');
+const box3 = document.getElementById('box-3');
+const box4 = document.getElementById('box-4');
+const box5 = document.getElementById('box-5');
+const box6 = document.getElementById('box-6');
+const box7 = document.getElementById('box-7');
+const box8 = document.getElementById('box-8');
+const box9 = document.getElementById('box-9');
 
-
-let gameBoard = () => {
+const gameBoard = (() => {
+    const gameGrid = document.getElementById('game-grid');
     let gameBoardArr = ['','','','','','','','',''];
 
-    let drawInSquare = (gameSymbol, boxNum) => {
-        // if box is not drawn in add symbol
-        if (gameBoardArr[boxNum] == '') {
-            gameBoardArr[boxNum] = gameSymbol;
-        }
-    }
 
-    let resetBoard = () => {
-        // reset Game
-        gameBoardArr = ['','','','','','','','',''];
-    }
+})();
+
+function render() {
+    
 }
+
+// var game = gameBoard();
+
+render();
